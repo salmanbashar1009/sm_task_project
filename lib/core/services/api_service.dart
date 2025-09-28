cdc1652450d29b46211f2222115089a00938a5fa
salmanbashar1009 <salmanbashar101@gmail.com>
2025-09-28 17:26:30 +0600

    api service added

--- lib/core/services/api_service.dart
+++ lib/core/services/api_service.dart
@@ -1,7 +1,20 @@
+import 'dart:convert';
+
+import 'package:sm_task_project/core/models/post_model.dart';
+import 'package:http/http.dart' as http;
+
+import '../urls/urls.dart';
+
 class ApiService{
 
-  Future<void> fetchPosts() async {
-
+  Future<List<Post>> fetchPosts() async {
+    final response = await http.get(Uri.parse(Urls.postUrl));
+    if(response.statusCode == 200){
+      List<dynamic> body = json.decode(response.body);
+      return body.map((dynamic item) => Post.fromJson(item)).toList();
+    }else{
+      throw Exception('Failed to load posts');
+    }
   }
 
 }
--- lib/core/urls/urls.dart
+++ lib/core/urls/urls.dart
@@ -1,3 +1,6 @@
 class Urls{
   static const String baseUrl = 'https://jsonplaceholder.typicode.com';
+
+  /// post url
+  static const String postUrl = '$baseUrl/posts';
 }
