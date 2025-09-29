565b7d6090dda21e68975e17ff0fa038c498721b
salmanbashar1009 <salmanbashar101@gmail.com>
2025-09-29 13:32:42 +0600

    updated

--- lib/core/route_config/app_routes.dart
+++ lib/core/route_config/app_routes.dart
@@ -5,6 +5,7 @@ import 'package:sm_task_project/presentations/view/sign_in_screen/sign_in_screen
 import 'package:sm_task_project/presentations/view/sign_up_screen/sign_up_screen.dart';
 import '../../presentations/view/post_screen/post_screen.dart';
 import '../../presentations/view/splash_screen/splash_screen.dart';
+import '../state_controller_binders/state_controller_binder.dart';
 
 
 class AppRoutes {
@@ -17,21 +18,25 @@ class AppRoutes {
       name: RouteNames.postScreen,
       page: () => PostScreen(),
       transition: Transition.rightToLeft,
+      binding: StateControllerBinder(),
     ),
     GetPage(
       name: RouteNames.onBoardingScreen,
       page: () => OnboardingScreen(),
       transition: Transition.noTransition,
+      binding: StateControllerBinder(),
     ),
     GetPage(
       name: RouteNames.signInScreen,
       page: () => SignInScreen(),
       transition: Transition.rightToLeft,
+      binding: StateControllerBinder()
     ),
     GetPage(
       name: RouteNames.signUpScreen,
       page: () => SignUpScreen(),
       transition: Transition.rightToLeft,
+      binding: StateControllerBinder()
     ),
   ];
 }
